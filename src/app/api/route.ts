82921e5378ffbaaaef0130af6ac8755219da4645
Péter Tombor <public@tmbpeter.com>
2025-02-15 23:55:51 +0100

    feat: display user's name in sample API response

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,7 +2,7 @@ import { auth } from "@/auth";
 
 export async function GET(request: Request) {
     const session = await auth();
-    return new Response("Hello, world!" + session, {
+    return new Response("Hello, world!" + session?.user?.name, {
         headers: { "content-type": "text/plain" },
     });
 }
