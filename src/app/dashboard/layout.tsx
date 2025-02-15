1eb5c7555c36810d347f313e91b858724a89f4d2
Péter Tombor <public@tmbpeter.com>
2025-02-15 23:10:26 +0100

    feat: initial appshell

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,60 @@
+"use client";
+
 import SignOutButton from "@/components/signoutButton";
+import {
+  AppShell,
+  Burger,
+  AppShellHeader,
+  Group,
+  Skeleton,
+  AppShellNavbar,
+  AppShellMain,
+  Text,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
     <div>
-      <SignOutButton />
-      {children}
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: "sm",
+          collapsed: { mobile: !opened },
+        }}
+        padding="md"
+      >
+        <AppShellHeader>
+          <Group h="100%" px="md">
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="sm"
+            />
+            <Text size="xl">HibaVonal</Text>
+            <div style={{ marginLeft: "auto" }}>
+              <SignOutButton />
+            </div>
+          </Group>
+        </AppShellHeader>
+        <AppShellNavbar p="md">
+          Navbar
+          {Array(15)
+            .fill(0)
+            .map((_, index) => (
+              <Skeleton key={index} h={28} mt="sm" animate={false} />
+            ))}
+        </AppShellNavbar>
+        <AppShellMain>{children}</AppShellMain>
+      </AppShell>
     </div>
   );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,24 @@
 import { auth } from "@/auth";
+import { Text } from "@mantine/core";
 
 export default async function DashboardPage() {
   const session = await auth();
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome, {session?.user?.name}</p>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "90vh",
+        flexDirection: "column",
+      }}
+    >
+      <Text style={{ textAlign: "center", fontSize: "4rem" }}>HibaVonal</Text>
+      <Text style={{ textAlign: "center", fontSize: "2rem" }}>
+        {session?.user?.name}
+        {" is logged in"}
+      </Text>
     </div>
   );
 }
