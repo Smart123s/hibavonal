8ce094a59c8a4e08aec5f3ddbc4cf8c5f6262f4a
Péter Tombor <public@tmbpeter.com>
2025-02-16 19:50:08 +0100

    refactor: move dashboard auth check to server
    Improves performance
    Removes flashing render of incomplete dashboard

---
+++ src/app/dashboard/clientLayout.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import SignOutButton from "@/components/signoutButton";
+import {
+  AppShell,
+  Burger,
+  AppShellHeader,
+  Group,
+  AppShellNavbar,
+  AppShellMain,
+  Text,
+  NavLink,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export default function DashboardClientLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [opened, { toggle }] = useDisclosure();
+  const [activeNav, setActiveNav] = useState("");
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setActiveNav(window.location.pathname.split("/")[2]);
+    }
+  }, []);
+
+  const linksData = ["Home", "Tickets"];
+
+  const links = linksData.map((link) => (
+    <NavLink
+      href={`/dashboard/${link.toLowerCase()}`}
+      label={link}
+      active={activeNav === link.toLowerCase()}
+      onClick={() => setActiveNav(link.toLowerCase())}
+      key={link}
+      component={Link}
+    />
+  ));
+
+  return (
+    <div>
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: "sm",
+          collapsed: { mobile: !opened },
+        }}
+        padding="md"
+      >
+        <AppShellHeader>
+          <Group h="100%" px="md">
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="sm"
+            />
+            <Text size="xl">HibaVonal</Text>
+            <div style={{ marginLeft: "auto" }}>
+              <SignOutButton />
+            </div>
+          </Group>
+        </AppShellHeader>
+        <AppShellNavbar p="md">{links}</AppShellNavbar>
+        <AppShellMain>{children}</AppShellMain>
+      </AppShell>
+    </div>
+  );
+}
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,84 +1,14 @@
-"use client";
-import { useSession } from "next-auth/react";
+import RequiresAuth from "../components/requiresAuth";
+import DashboardClientLayout from "./clientLayout";
 
-import SignOutButton from "@/components/signoutButton";
-import {
-  AppShell,
-  Burger,
-  AppShellHeader,
-  Group,
-  AppShellNavbar,
-  AppShellMain,
-  Text,
-  NavLink,
-} from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-import { redirect } from "next/navigation";
-
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [opened, { toggle }] = useDisclosure();
-  const [activeNav, setActiveNav] = useState("");
-  const { status } = useSession();
-
-  useEffect(() => {
-    if (status === "unauthenticated") {
-      redirect("/auth/login");
-    }
-  }, [status]);
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setActiveNav(window.location.pathname.split("/")[2]);
-    }
-  }, []);
-
-  const linksData = ["Home", "Tickets"];
-
-  const links = linksData.map((link) => (
-    <NavLink
-      href={`/dashboard/${link.toLowerCase()}`}
-      label={link}
-      active={activeNav === link.toLowerCase()}
-      onClick={() => setActiveNav(link.toLowerCase())}
-      key={link}
-      component={Link}
-    />
-  ));
-
   return (
-    <div>
-      <AppShell
-        header={{ height: 60 }}
-        navbar={{
-          width: 300,
-          breakpoint: "sm",
-          collapsed: { mobile: !opened },
-        }}
-        padding="md"
-      >
-        <AppShellHeader>
-          <Group h="100%" px="md">
-            <Burger
-              opened={opened}
-              onClick={toggle}
-              hiddenFrom="sm"
-              size="sm"
-            />
-            <Text size="xl">HibaVonal</Text>
-            <div style={{ marginLeft: "auto" }}>
-              <SignOutButton />
-            </div>
-          </Group>
-        </AppShellHeader>
-        <AppShellNavbar p="md">{links}</AppShellNavbar>
-        <AppShellMain>{children}</AppShellMain>
-      </AppShell>
-    </div>
+    <RequiresAuth>
+      <DashboardClientLayout>{children}</DashboardClientLayout>
+    </RequiresAuth>
   );
 }
