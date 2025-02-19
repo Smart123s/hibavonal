a359ce97b46944b4e869a4c390cb50c89a204eb0
Péter Tombor <public@tmbpeter.com>
2025-02-19 14:32:35 +0100

    feat: view ticket page

--- src/app/dashboard/tickets/[id]/page.tsx
+++ src/app/dashboard/tickets/[id]/page.tsx
@@ -1,12 +1,43 @@
+import { auth } from "@/auth";
+import { prisma } from "@/prisma";
+import { Ticket } from "@prisma/client";
+import { Card, Text, Badge } from "@mantine/core";
+
 export default async function ViewTicketPage({
   params,
 }: {
-  params: Promise<{ id: number }>;
+  params: Promise<{ id: string }>;
 }) {
   //TODO: check ijf authenticated user is allowed to view this ticket
+  const session = await auth();
+
+  const ticket: Ticket | null = await prisma.ticket.findUnique({
+    where: { id: (await params).id },
+  });
+
+  if (!ticket) {
+    return <h1>Ticket not found</h1>;
+  }
+
+  if (ticket.userId !== session?.user?.id) {
+    return <h1>Unauthorized</h1>;
+  }
+
   return (
-    <div>
-      <h1>{(await params).id}</h1>
-    </div>
+    <Card shadow="sm" padding="lg" radius="md" withBorder>
+      <div
+        style={{ display: "flex", alignItems: "center", marginBottom: "1rem" }}
+      >
+        <Text size="lg" style={{ marginRight: "1rem" }}>
+          {ticket.title}
+        </Text>
+        <Badge color={"red"} variant="light">
+          {"STATUS"}
+        </Badge>
+      </div>
+      <Text size="sm" color="dimmed">
+        {ticket.description}
+      </Text>
+    </Card>
   );
 }
